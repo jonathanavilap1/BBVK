7632f0b8e97eb138ea877c23c4bd262b03d18a77
jonathanavilap1 <40183554+jonathanavilap1@users.noreply.github.com>
2022-02-20 19:11:38 -0600

    First Commit

---
+++ BBVK/BBVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BBVK
+//
+//  Created by Jonathan Avila Perez on 20/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
