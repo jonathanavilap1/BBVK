bfea1fa04f362bd2b014e5c29c61fc74501e0957
jonathanavilap1 <40183554+jonathanavilap1@users.noreply.github.com>
2022-02-23 19:48:20 -0600

    adding automatization of buttons and labels

---
+++ BBVK/Utilities/BBVKUtilities.swift
@@ -0,0 +1,128 @@
+import UIKit
+import Foundation
+class initializerUI{
+    var stringPlaceHolder: UITextField?
+    var placeHolder: NSAttributedString?
+
+    var uiImage: UIImageView?
+    var uiLabel: UILabel?
+    var uiButton: UIButton?
+    var uiView: UIView?
+    var alert: UIAlertController?
+    
+    
+    
+    //MARK: creation of images
+     func uiImageViewSetter ( uiImageName: String) -> UIImageView{
+        uiImage = UIImageView()
+        uiImage?.image = UIImage(named: uiImageName)
+        
+        return uiImage!
+    }
+    
+    //MARK: creation of labels
+     func uiLabelSetter ( labelString: String, labelSize: CGFloat,  textaligment: NSTextAlignment,  isBold: Bool, isHighLighted: Bool) -> UILabel{
+        uiLabel = UILabel()
+        uiLabel?.font = UIFont(name: "HelveticaNeue", size: labelSize)
+        if isBold == true {
+            uiLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: labelSize)
+        }
+        if isHighLighted == true {
+           uiLabel?.highlightedTextColor = constants.textColorHighligthed
+            uiLabel?.isHighlighted = true
+        }
+        
+        uiLabel?.text = labelString
+        uiLabel?.textColor = constants.textColor
+        uiLabel?.textAlignment = textaligment
+        
+        return uiLabel!
+    }
+    
+    //MARK: creation of placeHolders
+     func textFieldSetter( isClear: Bool, placeHolderString: String, isSecure: Bool) -> UITextField {
+        
+        stringPlaceHolder = UITextField()
+        stringPlaceHolder?.textColor = .black
+        if isClear != true {
+            stringPlaceHolder?.backgroundColor = .white
+            stringPlaceHolder?.layer.borderWidth = 1
+        }
+        
+        if isSecure == true{
+            stringPlaceHolder?.isSecureTextEntry = true
+        }
+        
+        stringPlaceHolder?.attributedPlaceholder = NSAttributedString(string: placeHolderString ,
+                                                                      attributes: [NSAttributedString.Key.foregroundColor: constants.placeHolderColor])
+        return stringPlaceHolder!
+    }
+    
+    
+    //MARK: creation of buttons
+    
+   func uiButtonSetter(ispurple:Bool, isgray:Bool, isgreen: Bool) -> UIButton{
+        
+        uiButton = UIButton()
+      uiButton?.layer.cornerRadius = 5
+      uiButton?.titleLabel?.font = UIFont(name: "HelveticaNeue", size: 18)
+
+        
+        if ispurple == true {
+           uiButton?.backgroundColor = constants.backgroundButtoncolorpurple
+           uiButton?.setTitleColor(.white, for: .normal)
+        }
+        
+      if isgray == true {
+         uiButton?.backgroundColor = constants.backgroundButtoncolorgray
+         uiButton?.setTitleColor(.white, for: .normal)
+      }
+        
+      if isgreen == true {
+         uiButton?.backgroundColor = constants.backgroundButtoncolorGreen
+         uiButton?.setTitleColor(.white, for: .normal)
+      }
+        
+        
+        return uiButton!
+    }
+    
+    
+    //MARK: Alert creation
+    
+    func alertViewSetter(tittle: String, message: String, buttontittle: String?) -> UIAlertController{
+        
+        let alert = UIAlertController(title: tittle, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: buttontittle, style: UIAlertAction.Style.default, handler: nil))
+        
+        return alert
+    }
+    
+//    func stackViewSetter() ->UIStackView{
+//        let stackView = UIStackView()
+//        stackView.axis  = NSLayoutConstraint.Axis.horizontal
+//        stackView.distribution  = UIStackView.Distribution.equalSpacing
+//        stackView.alignment = UIStackView.Alignment.center
+//        stackView.spacing   = 16.0
+//
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        stackView.isLayoutMarginsRelativeArrangement = true
+//        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+//        stackView.layer.cornerRadius = 10
+//        stackView.layer.borderColor = headerInit.backgroundButtoncolor.cgColor
+//        stackView.backgroundColor = headerInit.backgroundButtoncolor
+//
+//
+//
+//        return stackView
+//    }
+   
+ 
+    
+    
+    
+    
+}
+
+
---
+++ BBVK/Utilities/constants.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+import Foundation
+
+class constants{
+static let backgroundButtoncolorGreen = UIColor(red: 0.00, green: 0.80, blue: 0.80, alpha: 1.00)
+static let backgroundButtoncolorgray = UIColor(red: 0.50, green: 0.50, blue: 0.50, alpha: 1.00)
+static let backgroundButtoncolorpurple = UIColor(red: 0.48, green: 0.47, blue: 0.92, alpha: 1.00)
+   
+static let placeHolderColor = UIColor(displayP3Red: 61/255, green: 68/255, blue: 76/255, alpha: 0.5)
+static let textColorHighligthed = UIColor(displayP3Red: 50/255, green: 160/255, blue: 168/255, alpha: 1)
+   static let viewBackGroundColor = UIColor(displayP3Red: 89/255, green: 194/255, blue: 230/255, alpha: 1)
+}
